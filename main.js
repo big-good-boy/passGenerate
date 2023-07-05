0ad4ff49ed0e9bb8553a62f6581b26d46c078aa9
big-good-boy <drago999@bk.ru>
2023-07-05 12:55:32 +0500

    добавлен функционал

--- main.js
+++ main.js
@@ -1,19 +1,63 @@
+const upp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const low = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const sym = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '.', '/', ':', ';', '=', '>', '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}', '~', '\'', '-', '<', '_', '>'];
+
 function generate() {
-	const basSym = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-
-	const specSym = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '.', '/', ':', ';', '=', '>', '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}', '~', '\'', '-', '<', '_', '>'];
-
-	let arr = basSym;
-	// let arr = basSym.concat(specSym);
-
-	let random;
-
-	let result = '';
-
-	for (let index = 0; index < document.getElementById('length').value; index++) {
-		random = Math.floor(Math.random() * arr.length);
-		result += arr[random];
+	let pass;
+	let libSym = new Array();
+	if (document.getElementById('symbol').checked) {
+		libSym = libSym.concat(upp, low, num, sym);
+		pass = password(libSym);
+		if (parser(pass, upp)
+		 && parser(pass, low)
+		 && parser(pass, num)
+		 && parser(pass, sym)) {
+			document.querySelector('.pass').textContent = pass.join('');
+		} else {
+			generate();
+		}
+	} else {
+		libSym = libSym.concat(upp, low, num);
+		pass = password(libSym);
+		if (parser(pass, upp)
+		 && parser(pass, low)
+		 && parser(pass, num)) {
+			document.querySelector('.pass').textContent = pass.join('');
+		} else {
+			generate();
+		}
 	}
-
-	document.querySelector('.pass').textContent = result;
+	if (document.getElementById('whitespace').checked) {
+		addSpace(pass);
+		document.querySelector('.pass').textContent = pass.join('');
+	}
 }
+
+function password(arr) {
+	let passArr = new Array();
+	for (let i = 0; i < document.getElementById('length').value; i++) {
+		passArr.push(arr[Math.floor(Math.random() * arr.length)]);
+	}
+	return passArr;
+}
+
+function parser(arr, lib) {
+	for (const i of lib) {
+		for (const ii of arr) {
+			if (ii === i) {
+				return true;
+			}
+		}
+	}
+}
+
+function addSpace(arr) {
+	arr[Math.floor(Math.random() * (arr.length - 2) + 1)] = ' ';
+}
+
+function copy() {
+	window.navigator.clipboard.writeText(document.querySelector('.pass').textContent);
+}
+
+generate();
